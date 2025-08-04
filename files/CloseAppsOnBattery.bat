e8e71ed34abbbf2d58361efa06533dd959e4569d
Rahbab <58372697+mrc2rules@users.noreply.github.com>
2025-08-04 20:46:01 +0300

    Add new applications

--- files/CloseAppsOnBattery.bat
+++ files/CloseAppsOnBattery.bat
@@ -8,9 +8,20 @@ call msgbox "Battery mode detected.\nClose background apps?"  "4+64" "Battery Mo
 if errorlevel = 7 (
         exit
     ) else (
-        taskkill /IM steam.exe /F
+		::Productivity
+		taskkill /IM spotify.exe /F
+		taskkill /IM teams.exe /F
+		taskkill /IM zoom.exe /F
+		taskkill /IM onedrive.exe /F
 		taskkill /IM discord.exe /F
+		::Gaming
+        taskkill /IM steam.exe /F
 		taskkill /IM epicgameslauncher.exe /F
+		taskkill /IM EADesktop.exe /F
+		taskkill /IM EALauncher.exe /F
+		taskkill /IM EABackgroundService.exe /F
 		taskkill /FI "IMAGENAME eq NVIDIA Broadcast.exe" /F
+		taskkill /IM riotclientservices.exe /F
+
 		exit
     )
